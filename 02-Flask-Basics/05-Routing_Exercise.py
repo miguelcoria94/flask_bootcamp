0d1009823b6263844d40c223aa7304636f8bef5c
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-04 17:15:32 -0600

    working on templates

--- 02-Flask-Basics/05-Routing_Exercise.py
+++ 02-Flask-Basics/05-Routing_Exercise.py
@@ -1,22 +1,29 @@
 # Set up your imports here!
 # import ...
+from flask import Flask
+app = Flask(__name__)
 
 
-@app.route('') # Fill this in!
+@app.route('/') # Fill this in!
 def index():
     # Welcome Page
     # Create a generic welcome page.
-    pass
+    return '<h1>Welcome Page</h1>'
 
-@app.route('') # Fill this in!
+@app.route('/puppy/<name>') # Fill this in!
 def puppylatin(name):
     # This function will take in the name passed
     # and then use "puppy-latin" to convert it!
 
+    if name[len(name) - 1] != 'y':
+        newname = name + "y"
+    else:
+        first_half = name[:-1]
+        newname = first_half + "iful"
     # HINT: Use indexing and concatenation of strings
     # For Example: "hello"+" world" --> "hello world"
-    pass
+    return '<h1>your new name is {}<h1>'.format(newname)
 
 if __name__ == '__main__':
     # Fill me in!
-    pass
+    app.run(debug=True)
--- 03-Template-Basics/00-Basic-Template.py
+++ 03-Template-Basics/00-Basic-Template.py
@@ -1,11 +1,2 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    # Connecting to a template (html file)
-    return render_template('00-Basic-Template.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
