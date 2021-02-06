c289e7bd264ce99acf81c62de2d7b02b6363c6b1
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-05 22:30:32 -0600

    control flow

--- 03-Template-Basics/00-Basic-Template.py
+++ 03-Template-Basics/00-Basic-Template.py
@@ -1,2 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('00-Basic-Template.html')
+
+if __name__ == '__main__':
+    app.run()
