75d762cfc3a66bef03b7218009668fd5f97f9142
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-08 16:32:22 -0600

    done with form

--- 04-Forms/01-Form-Fields.py
+++ 04-Forms/01-Form-Fields.py
@@ -12,4 +12,6 @@ class InfoForm(FlaskForm):
     breed = StringField('What breed are you?', validators=[DataRequired()])
     neutered = BooleanField("Has this dog been neatured?")
     mood = RadioField('Please choose your mood:', choices=[("mood_one", 'Happy'), ('mood_two', 'Excited')])
-    
+    food_choice = SelectField(u'Pick your favorite food:', choices=[('chi', 'Chicken'), ('bf', 'Beef'), ('fish', 'Fish')])
+    feedback = TextAreaField()
+    submit = SubmitField('Submit')
