f647ebbf779b78fa43776f3c8ef01bd2f9bdca1d
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-08 16:08:29 -0600

    building form

--- 04-Forms/01-Form-Fields.py
+++ 04-Forms/01-Form-Fields.py
@@ -1,1 +1,15 @@
 from flask import Flask, render_template, session, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import (StringField, BooleanField, DateTimeField,RadioField, SelectField, TextField, TextAreaField, SubmitField)
+
+from wtforms.validators import DataRequired, data_required
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mykey'
+
+class InfoForm(FlaskForm):
+    breed = StringField('What breed are you?', validators=[DataRequired()])
+    neutered = BooleanField("Has this dog been neatured?")
+    mood = RadioField('Please choose your mood:', choices=[("mood_one", 'Happy'), ('mood_two', 'Excited')])
+    
