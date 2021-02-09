8b059c71915961122685b16e7cbc6b58d1872736
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-09 16:32:16 -0600

    doing crud

--- miky-db-practice/BasicCRUD.py
+++ miky-db-practice/BasicCRUD.py
@@ -6,3 +6,21 @@ db.session.commit()
 
 ## print(my_puppy)
 
+# all_puppies = Puppy.query.all()
+# print(all_puppies)
+
+# puppy_one = Puppy.query.get(1)
+
+# puppy_sam = Puppy.query.filter_by(name="Sammy")
+
+# first_puppy = Puppy.query.get(1)
+# first_puppy.age = 10
+# db.session.add(first_puppy)
+# db.session.commit()
+
+
+second_puppy = Puppy.query.get(2)
+db.session.delete(second_puppy)
+db.session.commit()
+
+
