731f19adabe1ecb8f4c439cf3752fed09c60dd17
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-10 11:36:50 -0600

    starting passwords

--- website/adoption_site.py
+++ website/adoption_site.py
@@ -1,5 +1,5 @@
 import os
-from forms import AddForm, DelFrom
+from forms import AddForm, DelForm
 
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
@@ -38,4 +38,36 @@ def index():
 
 @app.route("/add", methods=['GET', 'POST'])
 def add_pup():
-    form = AddForm
+    form = AddForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        new_pup = Puppy(name)
+        db.session.add(new_pup)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+    
+    return render_template('add.html', form=form)
+
+@app.route("/list")
+def list_pup():
+    puppies = Puppy.query.all()
+    return render_template('list.html', puppies=puppies)
+
+
+@app.route("/delete", methods=["GET", 'POST'])
+def del_pup():
+    form = DelForm()
+
+    if form.validate_on_submit():
+        id = form.id.data
+        pup = Puppy.query.get(id)
+        db.session.delete(pup)
+        db.session.commit()
+
+        return redirect(url_for("list_pup"))
+    return render_template('delete.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
