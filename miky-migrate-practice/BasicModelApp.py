cb5c463f5a4bf1ba165aab6e0c955921701aa063
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-09 18:06:54 -0600

    started relationship

--- miky-migrate-practice/BasicModelApp.py
+++ miky-migrate-practice/BasicModelApp.py
@@ -22,10 +22,12 @@ class Puppy(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     age = db.Column(db.Integer)
+    breed = db.Column(db.Text)
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, breed):
         self.name = name
         self.age = age
+        self.breed = breed
 
     def __repr__(self):
         return f"Puppy {self.name} is {self.age} years old"
